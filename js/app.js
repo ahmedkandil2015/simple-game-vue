acb135f334e277db1b4913efdf437617d52a15a7
Ahmed Kandil <ahmedkandil2015@hotmail.com>
2018-06-21 13:18:54 +0200

    create finished logs section and refector health function and give up

--- js/app.js
+++ js/app.js
@@ -4,22 +4,24 @@ data:{
     playerHealth:100,
     monsterHealth:100,
     gameIsRununing : false,
+    turns:[],
 },
 methods:{
  startGame : function(){
      this.gameIsRununing=true;
      this.playerHealth=100;
      this.monsterHealth=100;
+     this.turns=[];
  },
  attack:function(){
-    
+
     this.monsterAttack(10,3);
     this.playerAttack(12,5);
  }  ,
  spacialAttack:function(){
 
-    this.monsterAttack(20,10);
-    this.playerAttack(15,5);
+    this.monsterAttack(20,10,'Hard');
+    this.playerAttack(15,5,'Hard');
  },
  heal:function (){
     if(this.playerHealth <=90){
@@ -27,19 +29,26 @@ methods:{
     }else{
         this.playerHealth = 100;
     }
+    this.logs('Health' ,10 );
     this.playerAttack(12,5);
  },
  giveUp:function(){
 
+    this.gameIsRununing =false;
+
  },
- monsterAttack:function(max , min){
-    this.monsterHealth -= this.calculateDamage(max,min);
+ monsterAttack:function(max , min,spacial=''){
+     damage = this.calculateDamage(max,min);
+    this.monsterHealth -= damage;
+    this.logs(spacial,damage,false);
     if(this.chackwin()){
     return ;
     } 
 },
-playerAttack:function(max,min){
-    this.playerHealth -= this.calculateDamage(max,min);
+playerAttack:function(max,min,spacial=''){
+    var damage = this.calculateDamage(max,min);
+    this.playerHealth -= damage;
+    this.logs(spacial,damage,true);
     this.chackwin();
     return
 },
@@ -65,6 +74,12 @@ playerAttack:function(max,min){
         return true;
     }
     return false ;
+ },
+ logs:function (spacial='',damage,isplyer){
+    this.turns.unshift({
+        isPlayer:isplyer,
+        text:`Player Hits Monster ${spacial} for ` + damage,
+    });
  }
 }
 });
