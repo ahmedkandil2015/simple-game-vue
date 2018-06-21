b3574027b8face0963129b341932dd3e63480a9d
Ahmed Kandil <ahmedkandil2015@hotmail.com>
2018-06-21 11:41:41 +0200

    implement Spacial Attack in 2 plyer and refector function

--- js/app.js
+++ js/app.js
@@ -12,41 +12,53 @@ methods:{
      this.monsterHealth=100;
  },
  attack:function(){
-    var max = 10;
-    var min = 3 ;
-    var damage = Math.max(Math.floor(Math.random() * max ) + 1, min);
-    this.monsterHealth -= damage;
-
-    if(this.monsterHealth <= 0){
-        alert('Your Won !');
-        this.monsterHealth=100;
-        this.playerHealth=100;
-        this.gameIsRununing=false;
-        return;
-    }
-
-    var max = 12;
-    var min = 5 ;
-    var damage = Math.max(Math.floor(Math.random()* max ) + 1 , min)
-    this.playerHealth -= damage;
-
-    if(this.playerHealth <= 0){
-        alert('Your lost !');
-        this.playerHealth=100;
-        this.monsterHealth=100;
-        this.gameIsRununing=false;
-
-
-    }
+    
+    this.monsterAttack(10,3);
+    this.playerAttack(12,5);
  }  ,
  spacialAttack:function(){
 
+    this.monsterAttack(20,10);
+    this.playerAttack(15,5);
  },
  heal:function (){
 
  },
  giveUp:function(){
 
+ },
+ monsterAttack:function(max , min){
+    this.monsterHealth -= this.calculateDamage(max,min);
+    if(this.chackwin()){
+    return ;
+    } 
+},
+playerAttack:function(max,min){
+    this.playerHealth -= this.calculateDamage(max,min);
+    this.chackwin();
+},
+ calculateDamage:function (max , min){
+     return Math.max(Math.floor(Math.random() * max ) + 1, min);
+ },
+ chackwin:function (){
+    if(this.monsterHealth <= 0){
+        if(confirm('You won ! New Game ?')){
+            this.startGame();
+        }else{
+            this.gameIsRununing=false;
+        }
+        return true;
+    }
+    else if(this.playerHealth <=0){
+        if(confirm('Your lost ! New Game ?')){
+            this.startGame();
+        }else{
+
+            this.gameIsRununing=false;
+        }
+        return true;
+    }
+    return false ;
  }
 }
 });
